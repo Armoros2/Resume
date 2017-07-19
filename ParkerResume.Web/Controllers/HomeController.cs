f2c5c6c2c79c36020ff8cfebb0b8500a86401f13
Michael Shattuck <ms2474@gmail.com>
2017-07-18 19:27:59 -0600

    initial commit

---
+++ ParkerResume.Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace ParkerResume.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Resume()
+        {
+            return View();
+        }
+    }
+}
---
+++ ParkerResume.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ParkerResume.Web.Global" Language="C#" %>
